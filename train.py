cadc3594ff4c0d8e116eaecfc1cc65a5c1888955
JudePark96 <judepark@kookmin.ac.kr>
2020-05-20 05:45:56 +0900

    added cuda option to features

--- train.py
+++ train.py
@@ -180,16 +180,17 @@ def main():
     logger.info("  Num steps = %d", num_train_optimization_steps)
     num_train_step = num_train_optimization_steps
 
-    all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long)
-    all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long)
-    all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long)
-    all_start_positions = torch.tensor([f.start_position for f in train_features], dtype=torch.long)
-    all_end_positions = torch.tensor([f.end_position for f in train_features], dtype=torch.long)
+    all_input_ids = torch.tensor([f.input_ids for f in train_features], dtype=torch.long).to(device)
+    all_input_mask = torch.tensor([f.input_mask for f in train_features], dtype=torch.long).to(device)
+    all_segment_ids = torch.tensor([f.segment_ids for f in train_features], dtype=torch.long).to(device)
+    all_start_positions = torch.tensor([f.start_position for f in train_features], dtype=torch.long).to(device)
+    all_end_positions = torch.tensor([f.end_position for f in train_features], dtype=torch.long).to(device)
     train_data = TensorDataset(all_input_ids, all_input_mask, all_segment_ids,
                                all_start_positions, all_end_positions)
 
     train_sampler = RandomSampler(train_data)
-    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.train_batch_size)
+    # TODO => Add argument num_workers option
+    train_dataloader = DataLoader(train_data, sampler=train_sampler, batch_size=args.train_batch_size, num_workers=16)
 
     model.train()
     global_step = 0
